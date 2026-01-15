fe327170c3a4291a0375493f170886a92cc675f7
OofMini <miniisking@gmail.com>
2026-01-14 23:39:14 -0600

    Update Bagel.m

--- Bagel/Bagel.m
+++ Bagel/Bagel.m
@@ -3,13 +3,11 @@
 //  Bagel
 //
 //  Created by Chris Barker on 25/07/2020.
-//  Updated for iOS 15/16
+//  Updated for iOS 15 Scene Support
 //
 
 #import "Bagel.h"
 #import "UIView+AddConstraints.h"
-
-// MARK: Message Object
 
 @interface Message: NSObject
 @property NSString *message;
@@ -18,19 +16,13 @@
 
 @implementation Message
 -(id)initWithMessage:(NSString *)message forView:(UIView *)view {
-
     self = [super init];
     if( !self ) return nil;
-
     _message = message;
     _toView = view;
-
     return self;
-
 }
 @end
-
-// MARK: Bagel (tasty 🥯)
 
 @implementation Bagel
 
@@ -47,7 +39,6 @@ bool baking;
 }
 
 - (instancetype)init {
-
     self = [super init];
     if (self) {
         _backgroundColor = [UIColor purpleColor];
@@ -61,7 +52,6 @@ bool baking;
         messages = [[NSMutableArray alloc]init];
     }
     return self;
-
 }
 
 -(void)pop:(UIView * _Nullable) view withMessage:(NSString * _Nonnull) message {
@@ -71,37 +61,29 @@ bool baking;
 }
 
 -(void)sendMessage {
-
     Message *nextMessage = messages.firstObject;
-
     if (nextMessage == nil || [nextMessage.message isEqual: @""] || baking || messages.count == 0) {
         return;
     }
-
     [self makeBagel:nextMessage.toView withMessage:nextMessage.message withCompletion:^(bool complete) {
         [messages removeObjectAtIndex:0];
         [self sendMessage];
     }];
-
 }
 
 -(void)makeBagel:(UIView * _Nullable) view withMessage:(NSString * _Nonnull) message withCompletion:(void(^)(bool finished))completion {
-
     baking = true;
-
     UIView *viewToAdd = view;
     if (viewToAdd == nil) {
         viewToAdd = [self getKeyView];
     }
 
-    // Setup UIView 🍞
     UIView *bagelView = [[UIView alloc]init];
     [bagelView setBackgroundColor:[_backgroundColor colorWithAlphaComponent:0.98]];
     [bagelView setAlpha:0.0];
     [bagelView.layer setCornerRadius:15];
     [bagelView setClipsToBounds:YES];
 
-    // Setup UILabel 🏷
     UILabel *textLabel = [[UILabel alloc]init];
     [textLabel setTextColor:_textColor];
     [textLabel setTextAlignment:_textAlignment];
@@ -113,7 +95,6 @@ bool baking;
     [bagelView addSubview:textLabel];
     [viewToAdd addSubview:bagelView];
 
-    // Set Constaints 🏗
     [textLabel setTranslatesAutoresizingMaskIntoConstraints:NO];
     [bagelView setTranslatesAutoresizingMaskIntoConstraints:NO];
 
@@ -131,35 +112,20 @@ bool baking;
             completion(true);
         }];
     }];
-
 }
 
 -(UIView *)getKeyView {
-    // iOS 13+ / 15+ Scene Support
-    // We iterate over connected scenes to find the active foreground scene
+    // iOS 13+ / 15+ Scene Support (Fix for modern iOS)
     for (UIScene *scene in [UIApplication sharedApplication].connectedScenes) {
         if (scene.activationState == UISceneActivationStateForegroundActive && [scene isKindOfClass:[UIWindowScene class]]) {
             UIWindowScene *windowScene = (UIWindowScene *)scene;
-            // Find the key window in this scene
             for (UIWindow *window in windowScene.windows) {
-                if (window.isKeyWindow) {
-                    return window;
-                }
+                if (window.isKeyWindow) return window;
             }
         }
     }
-    
-    // Fallback for older iOS or if no scene matches
-    UIWindow *topWindow = [[[UIApplication sharedApplication].windows sortedArrayUsingComparator:^NSComparisonResult(UIWindow *firstWindow, UIWindow *secondWindow) {
-        return firstWindow.windowLevel - secondWindow.windowLevel;
-    }] lastObject];
-    
-    // If we have subviews, return the last one (often the top view controller's view)
-    if (topWindow.subviews.count > 0) {
-        return [[topWindow subviews] lastObject];
-    }
-    
-    return topWindow;
+    // Fallback
+    return [[UIApplication sharedApplication].windows lastObject];
 }
 
 @end
